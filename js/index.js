3d253cd32ef8ff9faafd5fa046ea9d717a5c8f50
saesgo <130008323+saesgo@users.noreply.github.com>
2024-02-29 01:54:36 -0500

    rewiund

--- js/index.js
+++ js/index.js
@@ -13,24 +13,24 @@ const nuevoTurnoForm = document.getElementById("nuevoTurnoForm");
 const formularioTurno = document.getElementById("formularioTurno");
 
 async function getUltimoID() {
-    try {
-        const response = await gapi.client.sheets.spreadsheets.values.get({
-            spreadsheetId: '16nyuvP5Y4TmHjLnPAknJJIlQOBY5bXoa7imKKOn4BYQ',
-            range: 'Turnos!A1:G',
-        });
+  try {
+    const response = await gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: '16nyuvP5Y4TmHjLnPAknJJIlQOBY5bXoa7imKKOn4BYQ',
+      range: 'Turnos!A:E',
+    });
 
-        const range = response.result;
-        if (!range || !range.values || range.values.length === 0) {
-            return 1; // Si no hay datos, devuelve 1 como primer ID
-        }
-
-        // Obtiene el último ID en la última fila
-        const ultimoID = parseInt(range.values[range.values.length - 1][0]);
-        return ultimoID + 1; // Devuelve el siguiente ID
-    } catch (err) {
-        console.error(err);
-        return null;
+    const range = response.result;
+    if (!range || !range.values || range.values.length === 0) {
+      return 1; // Si no hay datos, devuelve 1 como primer ID
     }
+
+    // Obtiene el último ID en la última fila
+    const ultimoID = parseInt(range.values[range.values.length - 1][0]);
+    return ultimoID + 1; // Devuelve el siguiente ID
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 }
 
 async function agregarNuevoTurno(turno) {
@@ -38,61 +38,62 @@ async function agregarNuevoTurno(turno) {
   turno.id = id.toString(); // Convierte el ID a cadena de texto y lo asigna al turno
 
   const update = [
-      turno.id,
-      turno.evaluador,
-      turno.tituloProblema,
-      turno.descripcionProblema,
-      turno.imagen  // Usamos directamente la URL de la imagen
+    turno.id,
+    turno.cliente,
+    turno.evaluador,
+    turno.tituloProblema,
+    turno.descripcionProblema,
+    turno.imagen
   ];
 
   try {
-      const response = await gapi.client.sheets.spreadsheets.values.append({
-          spreadsheetId: '16nyuvP5Y4TmHjLnPAknJJIlQOBY5bXoa7imKKOn4BYQ',
-          range: `Turnos!A1:E`,
-          valueInputOption: "USER_ENTERED",
-          insertDataOption: "INSERT_ROWS",
-          values: [update],
-      });
+    const response = await gapi.client.sheets.spreadsheets.values.append({
+      spreadsheetId: '16nyuvP5Y4TmHjLnPAknJJIlQOBY5bXoa7imKKOn4BYQ',
+      range: `Turnos!A:E`,
+      valueInputOption: "USER_ENTERED",
+      insertDataOption: "INSERT_ROWS",
+      values: [update],
+    });
 
-      return response;
+    return response;
   } catch (err) {
-      console.error(err);
-      return null;
+    console.error(err);
+    return null;
   }
 }
 
 function createTarjeta(turno, index) {
-    const nuevaTarjeta = document.createElement("div");
-    nuevaTarjeta.classList = "tarjeta";
-    nuevaTarjeta.innerHTML = `
-        <p>Título : ${turno.tituloProblema}</p>
-        <p>ID: ${turno.id}</p>
-        <p>Evaluador: ${turno.evaluador}</p>
-        <p>Descripción : ${turno.descripcionProblema}</p>
-        <img src="${turno.imagen}" alt="Imagen del problema">
-    `;
-    nuevaTarjeta.addEventListener("click", () => actualizarDetalle(index));
-    turnosContainer.appendChild(nuevaTarjeta);
+  const nuevaTarjeta = document.createElement("div");
+  nuevaTarjeta.classList = "tarjeta";
+  nuevaTarjeta.innerHTML = `
+    <p>Título : ${turno.tituloProblema}</p>
+    <p>ID: ${turno.id}</p>
+    <p>Evaluador: ${turno.evaluador}</p>
+    <p>Descripción : ${turno.descripcionProblema}</p>
+    <img src="${turno.imagen}" alt="Imagen del problema">
+  `;
+  nuevaTarjeta.addEventListener("click", () => actualizarDetalle(index));
+  turnosContainer.appendChild(nuevaTarjeta);
 }
 
 function actualizarTarjetas() {
-    turnosContainer.innerHTML = "";
-    turnos.forEach((turno, i) => {
-        createTarjeta(turno, i);
-    });
+  turnosContainer.innerHTML = "";
+  turnos.forEach((turno, i) => {
+    createTarjeta(turno, i);
+  });
 }
 
 function actualizarDetalle(index) {
-    if (indiceSeleccionado !== undefined) turnosContainer.children[indiceSeleccionado].classList.toggle("seleccionado", false);
-    clienteElement.innerText = turnos[index].cliente;
-    idElement.innerText = turnos[index].id;
-    nombreEvaluadorElement.innerText = turnos[index].evaluador;
-    tituloProblemaElement.innerText = turnos[index].tituloProblema;
-    descripcionProblemaElement.innerText = turnos[index].descripcionProblema;
-    imagenElement.src = turnos[index].imagen;
-    detalleContainer.classList.toggle("escondido", false);
-    indiceSeleccionado = index;
-    turnosContainer.children[indiceSeleccionado].classList.toggle("seleccionado", true);
+  if (indiceSeleccionado !== undefined) turnosContainer.children[indiceSeleccionado].classList.toggle("seleccionado", false);
+  clienteElement.innerText = turnos[index].cliente;
+  idElement.innerText = turnos[index].id;
+  nombreEvaluadorElement.innerText = turnos[index].evaluador;
+  tituloProblemaElement.innerText = turnos[index].tituloProblema;
+  descripcionProblemaElement.innerText = turnos[index].descripcionProblema;
+  imagenElement.src = turnos[index].imagen;
+  detalleContainer.classList.toggle("escondido", false);
+  indiceSeleccionado = index;
+  turnosContainer.children[indiceSeleccionado].classList.toggle("seleccionado", true);
 }
 
 finalizar.addEventListener("click", () => marcarTerminado(indiceSeleccionado));
@@ -100,55 +101,40 @@ finalizar.addEventListener("click", () => marcarTerminado(indiceSeleccionado));
 async function marcarTerminado(i) {
   const updateTurno = turnos[i];
   updateTurno.comentario = comentarioElement.value;
-  updateTurno.fechaHora = new Date().toISOString(); // Agrega la fecha y hora actual al turno
-
   const filaAEditar = 0; // Define filaAEditar según sea necesario
+  const res = await editTurno(updateTurno.id, updateTurno, filaAEditar);
+    if (res.status === 200) {
+    turnos = turnos.filter(turno => turno.id !== updateTurno.id);
+    indiceSeleccionado = 0;
 
-  try {
-      const res = await editTurno(updateTurno.id, updateTurno, filaAEditar);
-      if (res.status === 200) {
-          // Filtrar el turno marcado como terminado
-          turnos = turnos.filter(turno => turno.id !== updateTurno.id);
-          indiceSeleccionado = 0;
+    // Ocultar las tarjetas marcadas
+    Array.from(turnosContainer.children).forEach((tarjeta, index) => {
+        if (index === indiceSeleccionado) {
+            tarjeta.classList.toggle("seleccionado", false);
+        }
+    });
 
-          // Ocultar las tarjetas marcadas
-          Array.from(turnosContainer.children).forEach((tarjeta, index) => {
-              if (index === indiceSeleccionado) {
-                  tarjeta.classList.toggle("seleccionado", false);
-              }
-          });
-
-          // Actualizar las tarjetas
-          await actualizarTarjetas();
-
-          // Ocultar el detalleContainer
-          detalleContainer.classList.toggle("escondido", true);
-
-          // Limpiar el campo de comentario
-          comentarioElement.value = "";
-      } else {
-          console.error("Error al marcar como terminado el turno:", res);
-      }
-  } catch (error) {
-      console.error("Error al marcar como terminado el turno:", error);
+    await actualizarTarjetas();
+    detalleContainer.classList.toggle("escondido", true);
+    comentarioElement.value = "";
   }
 }
 
 agregarTurnoButton.addEventListener("click", () => {
-    nuevoTurnoForm.classList.toggle("escondido");
+  nuevoTurnoForm.classList.toggle("escondido");
 });
 
 nuevoTurnoForm.addEventListener("submit", async (event) => {
-    event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+  event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
 
-    const formData = new FormData(formularioTurno);
-    const nuevoTurno = {};
+  const formData = new FormData(formularioTurno);
+  const nuevoTurno = {};
 
-    formData.forEach((value, key) => {
-        nuevoTurno[key] = value;
-    });
+  formData.forEach((value, key) => {
+      nuevoTurno[key] = value;
+  });
 
-    await agregarNuevoTurno(nuevoTurno); // Llama a la función para agregar el nuevo turno
-    nuevoTurnoForm.classList.toggle("escondido");
-    formularioTurno.reset(); // Reinicia el formulario después de agregar el turno
+  await agregarNuevoTurno(nuevoTurno); // Llama a la función para agregar el nuevo turno
+  nuevoTurnoForm.classList.toggle("escondido");
+  formularioTurno.reset(); // Reinicia el formulario después de agregar el turno
 });
--- js/sheets.js
+++ js/sheets.js
@@ -4,7 +4,7 @@ async function getTurnos() {
   try {
     const response = await gapi.client.sheets.spreadsheets.values.get({
       spreadsheetId: '16nyuvP5Y4TmHjLnPAknJJIlQOBY5bXoa7imKKOn4BYQ',
-      range: 'Turnos!A2:G', // Rango corregido para incluir las siete columnas
+      range: 'Turnos!A2:G', // Rango corregido para incluir las cinco columnas
     });
 
     const range = response.result;
@@ -18,9 +18,9 @@ async function getTurnos() {
       if (isNaN(parseInt(fila[0])) || fila[6] !== undefined) return;
       const nuevoTurno = {
         id: fila[0],
-        evaluador: fila[1],
-        tituloProblema: fila[2],
-        descripcionProblema: fila[3],
+        evaluador: fila[1], 
+        tituloProblema: fila[2], 
+        descripcionProblema: fila[3], 
         imagen: fila[4],
         fecha: fila[5],
         comentario: fila[6]
@@ -28,37 +28,29 @@ async function getTurnos() {
       turnos.push(nuevoTurno);
     });
     console.log(turnos);
-  } catch (err) {
+    } catch (err) {
     console.error(err);
   }
 }
 
+
 async function editTurno(id, contenido) {
+  const update = [
+    contenido.id,
+    contenido.evaluador,
+    contenido.tituloProblema,
+    contenido.descripcionProblema,
+    contenido.imagen,
+    new Date().toISOString(),
+    contenido.comentario
+  ];
+
+  
+
   try {
-    // Encuentra el índice del turno en el array de turnos
-    const index = turnos.findIndex(turno => turno.id === id);
-    if (index === -1) {
-      console.error('No se encontró el turno con el ID especificado');
-      return;
-    }
-
-    // Calcula el número de fila en Sheets (sumando 2 porque empezamos desde la fila 2)
-    const filaAEditar = index + 2;
-
-    const update = [
-      contenido.id,
-      contenido.evaluador,
-      contenido.tituloProblema,
-      contenido.descripcionProblema,
-      contenido.imagen,
-      new Date().toISOString(),
-      contenido.comentario
-    ];
-
-    // Actualiza solo la fila correspondiente en Sheets
     const response = await gapi.client.sheets.spreadsheets.values.update({
       spreadsheetId: '16nyuvP5Y4TmHjLnPAknJJIlQOBY5bXoa7imKKOn4BYQ',
-      range: `Turnos!A${filaAEditar}:G${filaAEditar}`,
+      range: `Turnos!A${filaAEditar}:G${filaAEditar}`, // Rango corregido para incluir las cinco columnas
       values: [update],
       valueInputOption: "USER_ENTERED"
     });
@@ -95,23 +87,23 @@ async function addTurno(turno) {
 
 async function getLastId() {
   try {
-    const response = await gapi.client.sheets.spreadsheets.values.get({
-      spreadsheetId: '16nyuvP5Y4TmHjLnPAknJJIlQOBY5bXoa7imKKOn4BYQ',
-      range: 'Turnos!A:E', 
-    });
+      const response = await gapi.client.sheets.spreadsheets.values.get({
+          spreadsheetId: '16nyuvP5Y4TmHjLnPAknJJIlQOBY5bXoa7imKKOn4BYQ',
+          range: 'Turnos!A:E', // Rango que abarca todas las columnas necesarias
+      });
 
-    const range = response.result;
-    if (!range || !range.values || range.values.length === 0) {
-      console.warn("No se encontraron valores");
-      return 0; // Si no hay valores, devolvemos 0 como el último ID
-    }
+      const range = response.result;
+      if (!range || !range.values || range.values.length === 0) {
+          console.warn("No se encontraron valores");
+          return 0; // Si no hay valores, devolvemos 0 como el último ID
+      }
 
-    // Obtenemos el último ID de la última fila
-    const lastRow = range.values[range.values.length - 1];
-    const lastId = parseInt(lastRow[0]);
-    return lastId;
+      // Obtenemos el último ID de la última fila
+      const lastRow = range.values[range.values.length - 1];
+      const lastId = parseInt(lastRow[0]);
+      return lastId;
   } catch (err) {
-    console.error(err);
-    return 0; // En caso de error, devolvemos 0 como el último ID
+      console.error(err);
+      return 0; // En caso de error, devolvemos 0 como el último ID
   }
 }
