f560349efadb5f2d409287992fc06a7fb35d5c28
saesgo <130008323+saesgo@users.noreply.github.com>
2024-03-10 09:16:59 -0500

    update imgur

--- js/imgur.js
+++ js/imgur.js
@@ -20,3 +20,23 @@ var settings = {
 $.ajax(settings).done(function (response) {
   console.log(response);
 });
+
+var form = new FormData();
+form.append("refresh_token", "{{e600474ca163ca9bcb77b60c5c30c0b1517a94dd}}");
+form.append("client_id", "{{ae16a4008387500}}");
+form.append("client_secret", "{{0d1eaed0da8d151b2a7593ddc49e32778f999e3b}}");
+form.append("grant_type", "refresh_token");
+
+var settings = {
+  "url": "https://api.imgur.com/oauth2/token",
+  "method": "POST",
+  "timeout": 0,
+  "processData": false,
+  "mimeType": "multipart/form-data",
+  "contentType": false,
+  "data": form
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
