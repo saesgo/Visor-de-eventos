2eec6ae0fb5d2918a6a1ae883faf2ca8a48bdb90
saesgo <130008323+saesgo@users.noreply.github.com>
2024-03-10 12:11:48 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -96,6 +96,7 @@ function actualizarDetalle(index) {
 
 finalizar.addEventListener("click", () => marcarTerminado(indiceSeleccionado));
 
+
 async function marcarTerminado(i) {
   const updateTurno = turnos[i];
   updateTurno.comentario = comentarioElement.value;
