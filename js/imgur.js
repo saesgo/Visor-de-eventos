cf9b6fad8466fb3370c499b38adf34b68f068092
saesgo <130008323+saesgo@users.noreply.github.com>
2024-03-10 12:37:51 -0500

    Update imgur.js

--- js/imgur.js
+++ js/imgur.js
@@ -1,14 +1,22 @@
-document.getElementById("file").addEventListener( "change" ,async Event => {
-  Event.preventDefault()
-  const headers = new Headers()
-  headers.append("Authorization","Client-ID ae16a4008387500")
-  const formData = new FormData();
-  formData.append( "image",event.target.files[0] )
-  const requestOptions={
-    method: "POST",
-    headers:headers,
-    body: formData,
-  }
-  const url = await fetch("https://api.imgur.com/3/image", requestOptions )
-  .then(response=>response.json()).data.link
-})
+var form = new FormData();
+form.append("image", fileInput.files[0], "GHJQTpX.jpeg");
+form.append("type", "image");
+form.append("title", "Simple upload");
+form.append("description", "This is a simple image upload in Imgur");
+
+var settings = {
+  "url": "https://api.imgur.com/3/image",
+  "method": "POST",
+  "timeout": 0,
+  "headers": {
+    "Authorization": "Client-ID {{ae16a4008387500}}"
+  },
+  "processData": false,
+  "mimeType": "multipart/form-data",
+  "contentType": false,
+  "data": form
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
