cc8a6b20cd4d5b2b3fad2363ccd4a23b29b67c22
saesgo <130008323+saesgo@users.noreply.github.com>
2024-03-10 09:56:33 -0500

    reupload imgur

--- js/imgur.js
+++ js/imgur.js
@@ -1,42 +1,14 @@
-var form = new FormData();
-form.append("image", fileInput.files[0], "GHJQTpX.jpeg");
-form.append("type", "image");
-form.append("title", "Simple upload");
-form.append("description", "This is a simple image upload in Imgur");
-
-var settings = {
-  "url": "https://api.imgur.com/3/image",
-  "method": "POST",
-  "timeout": 0,
-  "headers": {
-    "Authorization": "Client-ID {{ae16a4008387500}}"
-  },
-  "processData": false,
-  "mimeType": "multipart/form-data",
-  "contentType": false,
-  "data": form
-};
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
-
-var form = new FormData();
-form.append("refresh_token", "{{137351013aa16610fdec359bc6f36fa5e957caaa}}");
-form.append("client_id", "{{ae16a4008387500}}");
-form.append("client_secret", "{{0d1eaed0da8d151b2a7593ddc49e32778f999e3b}}");
-form.append("grant_type", "refresh_token");
-
-var settings = {
-  "url": "https://api.imgur.com/oauth2/token",
-  "method": "POST",
-  "timeout": 0,
-  "processData": false,
-  "mimeType": "multipart/form-data",
-  "contentType": false,
-  "data": form
-};
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
+document.getElementById("file").addEventListener( "change" ,async Event => {
+  Event.preventDefault()
+  const headers = new Headers()
+  headers.append("Authorization","Client-ID ae16a4008387500")
+  const formData = new FormData();
+  formData.append( "image",event.target.files[0] )
+  const requestOptions={
+    method: "POST",
+    headers:headers,
+    body: formData,
+  }
+  const url = await fetch("https://api.imgur.com/3/image", requestOptions )
+  .then(response=>response.json()).data.link
+})
